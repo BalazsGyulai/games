5fc8ee28b8ccfe520100648a98f5542178300986
BalazsGyulai <60194914+BalazsGyulai@users.noreply.github.com>
2021-08-09 21:16:28 +0200

    initial commit

---
+++ src/components/DobChoose.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Ki vagy?</h1>
+  <button>Játékos</button>
+  <button>Admin</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DobChoose'
+}
+</script>
+
+<style scoped>
+
+</style>
